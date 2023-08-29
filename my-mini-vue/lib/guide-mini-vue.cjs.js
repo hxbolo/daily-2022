cfa52d1ff1dec35c23d6398144f3c074d55c2b41
hx <964124947.qq.com>
2023-08-29 16:28:10 +0800

    初始化element

--- my-mini-vue/lib/guide-mini-vue.cjs.js
+++ my-mini-vue/lib/guide-mini-vue.cjs.js
@@ -2,6 +2,10 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
+const isObject = (val) => {
+    return val !== null && typeof val === 'object';
+};
+
 function creatComponentInstance(vnode) {
     const component = {
         vnode,
@@ -40,31 +44,65 @@ function finishComponentSetup(instance) {
 
 function render(vnode, container) {
     // patch
-    patch(vnode);
+    patch(vnode, container);
 }
 function patch(vnode, container) {
     // 处理组件
     // 判断vnode 是不是一个element
     // 是element处理 element
-    // processElement()
-    processComponent(vnode);
+    if (typeof vnode.type === 'string') {
+        processElement(vnode, container);
+    }
+    else if (isObject(vnode.type)) {
+        //组件 object
+        processComponent(vnode, container);
+    }
+}
+function processElement(vnode, container) {
+    // 初始话
+    mountElement(vnode, container);
+    // 更新
+}
+function mountElement(vnode, container) {
+    const el = document.createElement(vnode.type);
+    // string array
+    const { children } = vnode;
+    if (typeof children === 'string') {
+        el.textContent = children;
+    }
+    else if (Array.isArray(children)) {
+        mountChildren(vnode, el);
+    }
+    // props
+    const { props } = vnode;
+    console.log(vnode);
+    for (const key in props) {
+        const val = props[key];
+        el.setAttribute(key, val);
+    }
+    container.append(el);
+}
+function mountChildren(vnode, el) {
+    vnode.children.forEach((v) => {
+        patch(v, el);
+    });
 }
 function processComponent(vnode, container) {
     // 挂载组件
-    mountComponent(vnode);
+    mountComponent(vnode, container);
 }
 function mountComponent(vnode, container) {
     //创建组件实例
     const instance = creatComponentInstance(vnode);
     setupComponent(instance);
-    setupRenderEffect(instance);
+    setupRenderEffect(instance, container);
 }
 function setupRenderEffect(instance, container) {
     // 虚拟节点树
     const subTree = instance.render();
     // vnode -> patch
     // vnode -> element -> mounElement
-    patch(subTree);
+    patch(subTree, container);
 }
 
 function createVNode(type, props, children) {
@@ -83,7 +121,7 @@ function createApp(rootComponent) {
             // componet -> vnode
             // 所有的逻辑都会基于虚拟节点 做处理
             const vnode = createVNode(rootComponent);
-            render(vnode);
+            render(vnode, rootContainer);
         }
     };
 }
--- my-mini-vue/src/runtime-core/renderer.ts
+++ my-mini-vue/src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from './../shared/index'
 import { creatComponentInstance, setupComponent } from './component'
 
 export function render(vnode, container) {
@@ -9,8 +10,47 @@ function patch(vnode, container) {
   // 处理组件
   // 判断vnode 是不是一个element
   // 是element处理 element
-  // processElement()
-  processComponent(vnode, container)
+  if (typeof vnode.type === 'string') {
+    processElement(vnode, container)
+  } else if (isObject(vnode.type)) {
+    //组件 object
+    processComponent(vnode, container)
+  }
+}
+
+function processElement(vnode: any, container: any) {
+  // 初始话
+  mountElement(vnode, container)
+  // 更新
+}
+
+function mountElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type)
+
+  // string array
+  const { children } = vnode
+
+  if (typeof children === 'string') {
+    el.textContent = children
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el)
+  }
+
+  // props
+  const { props } = vnode
+  console.log(vnode)
+
+  for (const key in props) {
+    const val = props[key]
+    el.setAttribute(key, val)
+  }
+  container.append(el)
+}
+
+function mountChildren(vnode, el) {
+  vnode.children.forEach((v) => {
+    patch(v, el)
+  })
 }
 
 function processComponent(vnode: any, container: any) {
@@ -31,5 +71,5 @@ function setupRenderEffect(instance, container) {
   const subTree = instance.render()
   // vnode -> patch
   // vnode -> element -> mounElement
-  patch(subTree, container )
+  patch(subTree, container)
 }
