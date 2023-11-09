2b8de69785702f1a5bda32c3efcf8853eb1ee958
hx <964124947.qq.com>
2023-11-09 15:44:22 +0800

    解析element节点

--- my-mini-vue/src/compiler-core/src/parse.ts
+++ my-mini-vue/src/compiler-core/src/parse.ts
@@ -1,5 +1,8 @@
 import { NodeTypes } from './ast'
-
+const enum TagType {
+  Start,
+  End,
+}
 export function baseParse(content: string) {
   const context = CreatPaeserContext(content)
   return creatRoot(parseChildren(context))
@@ -9,12 +12,52 @@ function parseChildren(context) {
   const nodes: any = []
 
   let node
+
+  const s = context.source
   // 看看如果是 {{ 开头的话，那么就是一个插值， 那么去解析他
-  if (context.source.startsWith('{{')) {
+  if (s.startsWith('{{')) {
     node = parseInterpolation(context)
+  } else if (s[0] === '<') {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context)
+    }
   }
   nodes.push(node)
   return nodes
+}
+
+function parseElement(context: any) {
+  // 应该如何解析 tag 呢
+  // <div></div>
+
+  const element = parseTag(context, TagType.Start)
+
+  parseTag(context, TagType.End)
+
+  console.log(context.source)
+
+  return element
+}
+
+function parseTag(context, type: TagType) {
+  // 1. 先解析开始 tag
+
+  const match: any = /^<\/?([a-z][^\r\n\t\f />]*)/i.exec(context.source)
+
+  const tag = match[1]
+
+  console.log(match)
+
+  addvanceBy(context, match[0].length)
+  addvanceBy(context, 1)
+
+  if(type === TagType.End)return
+
+  // 2. 删除处理完成的代码
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  }
 }
 //解析差值
 function parseInterpolation(context) {
@@ -39,8 +82,6 @@ function parseInterpolation(context) {
 
   // 最后在让代码前进2个长度，可以把 }} 干掉
   addvanceBy(context, rawContentLength + closeDelimiter.length)
-
-  console.log('source', context.source, content, closeIndex)
 
   return {
     type: NodeTypes.INTERPOLATION,
--- my-mini-vue/src/compiler-core/tests/parse.spec.ts
+++ my-mini-vue/src/compiler-core/tests/parse.spec.ts
@@ -10,11 +10,22 @@ describe('parser', () => {
       const ast = baseParse('{{ message }}')
 
       expect(ast.children[0]).toStrictEqual({
-        type:  NodeTypes.INTERPOLATION,
+        type: NodeTypes.INTERPOLATION,
         content: {
-          type:  NodeTypes.SIMPLE_EXPRESSION,
+          type: NodeTypes.SIMPLE_EXPRESSION,
           content: `message`,
         },
+      })
+    })
+  })
+
+  describe('element', () => {
+    test('simple element div', () => {
+      const ast = baseParse('<div></div>')
+
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ELEMENT,
+        tag: 'div',
       })
     })
   })
